3d6d50e9d5ee14ba545a729650df1908f8a860f4
nevesbr6 <118855875+nevesbr6@users.noreply.github.com>
2024-08-21 14:54:54 -0300

    create a base for final-server

---
+++ src/main/java/net/vexmos/Net.java
@@ -0,0 +1,16 @@
+package net.vexmos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Net extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
---
+++ src/main/java/net/vexmos/Proxy.java
@@ -0,0 +1,17 @@
+package net.vexmos;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Proxy extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
